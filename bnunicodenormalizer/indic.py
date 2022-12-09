51819356011671a07666103b5d99ac048838cc50
mnansary <silentassasin0111@gmail.com>
2022-12-10 01:03:06 +0600

    v-0.1.2

--- bnunicodenormalizer/indic.py
+++ bnunicodenormalizer/indic.py
@@ -28,3 +28,27 @@ class IndicNormalizer(BaseNormalizer):
             
     
   
+    def cleanInvalidUnicodes(self):
+        # clean starts
+        while self.checkDecomp() and self.decomp[0] in self.lang.invalid_starts:
+            self.decomp=self.decomp[1:]         
+        # clean endings
+        
+        # if the last one is a connector (allow halant ending)
+        if self.decomp[-1] == self.lang.connector: 
+            self.decomp=self.decomp[:-1]
+            add_con=True
+        else:
+            add_con=False
+        
+        while self.checkDecomp() and self.decomp[-1] == self.lang.connector:
+            self.decomp=self.decomp[:-1]
+        
+        # (allow halant ending)
+        if add_con:self.decomp.append(self.lang.connector)
+        
+        if self.checkDecomp():
+            # clean invalid unicodes
+            for idx,d in enumerate(self.decomp):
+                if d not in self.valid:
+                    self.decomp[idx]=None 
